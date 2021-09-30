2e4da704f1a9d4759a7091b1ca5564b6e91bce27
aelu419 <alanrtao@outlook.com>
2021-09-30 03:51:45 -0400

    reinitialize fmod plugin

---
+++ Assets/Scripts/Song.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// 
+using FMODUnity;
+using FMOD.Studio;
+
+public class Song : ScriptableObject
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
